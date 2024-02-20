040890d6874521d7cfa1e5fa3face2ba8fd44143
Jay <jp6631@srmist.edu.in>
2024-02-20 23:23:46 +0530

    completing the app

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,23 @@
+
+P
+ app/src/main/AndroidManifest.xml,8/c/8c55c3ccc257e5907959013f99656e4c8ec3903e
+m
+=app/src/main/java/com/example/quizapp/QuizQuestionActivity.kt,9/5/95ac8e71c58408632e1ca10a70e7b75836eeb8fe
+e
+5app/src/main/java/com/example/quizapp/MainActivity.kt,a/8/a869e00108d77b07ed4a1ddcd095cfae0b2acb0a
+@
+build.gradle.kts,d/b/dbcff70658daf80b53ce624f6adcaa529df5ed8d
+F
+app/proguard-rules.pro,9/e/9e08934d811afe28fbc77aaa3c0d747b94348db9
+A
+gradle.properties,2/a/2afbb999f001938c88fa43fc2ef52abf0f8213e4
+X
+(gradle/wrapper/gradle-wrapper.properties,f/b/fbe448ebfc3eb2d4e308f6b8b043666f5b57235e
+@
+local.properties,0/7/0712df971a99ac4d2fccb8e0fb19f377f3374cca
+b
+2app/src/main/java/com/example/quizapp/Constants.kt,0/f/0f489fc7f9e68e1408761c7e8463752039c49c21
+a
+1app/src/main/java/com/example/quizapp/Question.kt,6/c/6c0720b74d25563b16d41340b2bce31ef818055d
+g
+7app/src/main/java/com/example/quizapp/ResultActivity.kt,e/d/ed14a1880b641bb9338656cd3ffd0a731ed54933
--- app/src/main/java/com/example/quizapp/QuizQuestionActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionActivity.kt
@@ -1,5 +1,6 @@
 package com.example.quizapp
 
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
@@ -9,33 +10,36 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 
 class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
 
 
-    private var mCurrentPosition:Int = 1
+    private var mCurrentPosition: Int = 1
     private var mQuestionsList: ArrayList<Question>? = null
     private var mSelectedOptionPosition: Int = 0
+    private var mUserName :String? = null
+    private var mCorrectAnswers :Int = 0
 
-    private var progressBar:ProgressBar? = null
-    private var tvProgress : TextView? = null
-    private var tvQuestion : TextView? = null
-    private var ivImage : ImageView? = null
+    private var progressBar: ProgressBar? = null
+    private var tvProgress: TextView? = null
+    private var tvQuestion: TextView? = null
+    private var ivImage: ImageView? = null
 
-    private var tvOptionOne : TextView? = null
-    private var tvOptionTwo : TextView? = null
-    private var tvOptionThree : TextView? = null
-    private var tvOptionFour : TextView? = null
-    private var btnSubmit : Button? = null
-
-
-
+    private var tvOptionOne: TextView? = null
+    private var tvOptionTwo: TextView? = null
+    private var tvOptionThree: TextView? = null
+    private var tvOptionFour: TextView? = null
+    private var btnSubmit: Button? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_question)
+
+        mUserName = intent.getStringExtra(Constants.USER_NAME)
+
         progressBar = findViewById(R.id.progressBar)
         tvProgress = findViewById(R.id.tv_progress)
         tvQuestion = findViewById(R.id.tv_question)
@@ -82,29 +86,29 @@ class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    private fun defaultOptionsView(){
+    private fun defaultOptionsView() {
         val options = ArrayList<TextView>()
-        tvOptionOne?.let { options.add(0,it) }
-        tvOptionTwo?.let { options.add(1,it) }
-        tvOptionThree?.let { options.add(3,it) }
-        tvOptionFour?.let { options.add(4,it) }
+        tvOptionOne?.let { options.add(0, it) }
+        tvOptionTwo?.let { options.add(1, it) }
+        tvOptionThree?.let { options.add(3, it) }
+        tvOptionFour?.let { options.add(4, it) }
 
-        for (option in options){
+        for (option in options) {
             option.setTextColor(Color.parseColor("#7A8089"))
             option.typeface = Typeface.DEFAULT_BOLD
-            option.background =ContextCompat.getDrawable(
+            option.background = ContextCompat.getDrawable(
                 this,
                 R.drawable.default_option_border_bg
             )
         }
     }
 
-    private fun selectedOptionView(tv:TextView,selectedOptionNum :Int){
+    private fun selectedOptionView(tv: TextView, selectedOptionNum: Int) {
         defaultOptionsView()
         mSelectedOptionPosition = selectedOptionNum
 
         tv.setTextColor(Color.parseColor("#363A43"))
-        tv.setTypeface(tv.typeface,Typeface.BOLD)
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
         tv.background = ContextCompat.getDrawable(
             this,
             R.drawable.selected_option_border_bg
@@ -112,24 +116,89 @@ class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(view: View?) {
-        when(view?.id){
+        when (view?.id) {
             R.id.tv_option_one -> {
-                tvOptionOne?.let { selectedOptionView(it,1) }
+                tvOptionOne?.let { selectedOptionView(it, 1) }
             }
+
             R.id.tv_option_two -> {
-                tvOptionTwo?.let { selectedOptionView(it,2) }
+                tvOptionTwo?.let { selectedOptionView(it, 2) }
             }
+
             R.id.tv_option_three -> {
-                tvOptionThree?.let { selectedOptionView(it,3) }
+                tvOptionThree?.let { selectedOptionView(it, 3) }
             }
+
             R.id.tv_option_four -> {
-                tvOptionFour?.let { selectedOptionView(it,4) }
+                tvOptionFour?.let { selectedOptionView(it, 4) }
+            }
+
+            R.id.btn_submit -> {
+                if (mSelectedOptionPosition == 0) {
+                    mCurrentPosition++
+
+                    when {
+                        mCurrentPosition <= mQuestionsList!!.size -> {
+                            setQuestion()
+                        }
+
+                        else -> {
+                            val intent = Intent(this, ResultActivity::class.java)
+                            intent.putExtra(Constants.USER_NAME, mUserName)
+                            intent.putExtra(Constants.CORRECT_ANSWERS, mCorrectAnswers)
+                            intent.putExtra(Constants.TOTAL_QUESTION, mQuestionsList?.size)
+                            startActivity(intent)
+                            finish()
+                        }
+                    }
+                } else {
+                    val question = mQuestionsList?.get(mCurrentPosition - 1)
+                    if (question!!.correctAnswer != mSelectedOptionPosition) {
+                        answerView(mSelectedOptionPosition, R.drawable.wrong_option_border_bg)
+                    }else{
+                        mCorrectAnswers++
+                    }
+                    answerView(question.correctAnswer, R.drawable.correct_option_border_bg)
+
+                    if (mCurrentPosition == mQuestionsList!!.size) {
+                        btnSubmit?.text = "FINISH"
+                    } else {
+                        btnSubmit?.text = "GO TO NEXT QUESTION"
+                    }
+                    mSelectedOptionPosition = 0
+                }
             }
         }
-//        R.id.btn_submit ->{
-//            //ihjwe rh
-//
-//        }
+    }
 
+
+    private fun answerView(answer: Int, drawableView: Int){
+        when (answer) {
+            1 -> {
+                tvOptionOne?.background = ContextCompat.getDrawable(
+                    this, drawableView
+                )
+            }
+
+            2 -> {
+                tvOptionTwo?.background = ContextCompat.getDrawable(
+                    this, drawableView
+                )
+            }
+
+            3 -> {
+                tvOptionThree?.background = ContextCompat.getDrawable(
+                    this, drawableView
+                )
+            }
+
+            4 -> {
+                tvOptionFour?.background = ContextCompat.getDrawable(
+                    this, drawableView
+                )
+            }
+        }
     }
 }
+
+
